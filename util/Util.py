698606120de12e17a6bfc51b0fa4f4a4feb1e98a
sc4599 <sc4599@163.com>
2016-01-13 11:17:28 +0800

    增加tcp服务器的配置信息

--- util/Util.py
+++ util/Util.py
@@ -1,16 +1,32 @@
 # coding:utf8
-import os,json
+import os, json
 
-#获取当前文件夹的绝对路径
-path = os.path.dirname(__file__)
+# 获取当前文件夹的绝对路径
 
-f = open(path+'/config.json')
-config = json.load(f)
+
+
+
 
 # 记录常用属性， redisIP 等。。。
 class Config(object):
     def __init__(self):
         # print config
+        self.reidsIP = ''
+        self.redisPORT = ''
+        self.redisAUTH = ''
+        self.mysqlIP = ''
+        self.mysqlPORT = ''
+        self.mysqlUSERNAME = ''
+        self.mysqlPASSWORD = ''
+        self.mysqlDATABASE = ''
+        self.socketIP = ''
+        self.socketIPPORT = ''
+        self.httpIP = ''
+        self.httpPORT = ''
+    def getINIT(self):
+        path = os.path.dirname(__file__)
+        f = open(path + '/config.json')
+        config = json.load(f)
         self.reidsIP = config['redis']['ip']
         self.redisPORT = config['redis']['port']
         self.redisAUTH = config['redis']['auth']
@@ -19,6 +35,10 @@ class Config(object):
         self.mysqlUSERNAME = config['mysql']['username']
         self.mysqlPASSWORD = config['mysql']['password']
         self.mysqlDATABASE = config['mysql']['database']
+        self.socketIP = config['socket']['ip']
+        self.socketIPPORT = config['socket']['port']
+        self.httpIP = config['http']['ip']
+        self.httpPORT = config['http']['port']
 
 if __name__ == '__main__':
     c = Config()
